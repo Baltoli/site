7a520c21e870ffd83efb111bbb868712d02f65f2
Bruce Collie <brucecollie82@gmail.com>
2018-12-03 12:10:49 +0000

    Finish tutorial

--- pages/post.js
+++ pages/post.js
@@ -1,22 +1,39 @@
 import Layout from '../components/MyLayout'
-import fetch from 'isomorphic-unfetch'
+import {withRouter} from 'next/router'
+import Markdown from 'react-markdown'
 
-const Post = (props) => (
+export default withRouter((props) => (
   <Layout>
-    <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
-    <img src={props.show.image.medium} />
+    <h1>{props.router.query.title}</h1>
+    <div className="markdown">
+      <Markdown source={`
+This is a blog post
+Yes. Links [link](/link)
+
+### Title
+For some content
+      `}/>
+    </div>
+
+    <style jsx global>{`
+      .markdown {
+        font-family: "Arial";
+      }
+
+      .markdown a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      .markdown a:hover {
+        opacity: 0.6;
+      }
+
+      .markdown h3 {
+        margin: 0;
+        padding: 0;
+        text-transform: uppercase;
+      }
+    `}</style>
   </Layout>
-)
-
-Post.getInitialProps = async function(context) {
-  const { id } = context.query
-  const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
-  const show = await res.json()
-
-  console.log(`Fetched show: ${show.name}`)
-
-  return { show }
-}
-
-export default Post
+))
